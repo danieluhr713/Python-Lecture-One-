57d3414a8169712caf56f4fbaa06697876066aee
Daniel <Uhr>
2021-06-07 20:22:18 -0500

    first commit

---
+++ Lecture1.py
@@ -0,0 +1,6 @@
+first_name = "Daniel"
+age = "25"
+second_name = "Rodrigo"
+print(f"Hello {first_name}, My name is {second_name}. I am currently {age} years old.")
+
+int(age) #changes from string to int
